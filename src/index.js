a9ed89fb7b7b18eb0a9ed71cb23c321944d7fb06
Thomas Tisch <t.tisch@live.com>
2021-06-11 10:08:40 +0200

    Update index.js
    Remove placeholder span

--- src/index.js
+++ src/index.js
@@ -311,10 +311,6 @@ module.exports = (
       : ` background-image: url('${placeholderImageData}'); background-size: cover;`
 
     let rawHTML = `
-  <span
-    class="${imageBackgroundClass}"
-    style="padding-bottom: ${ratio}; position: relative; bottom: 0; left: 0;${bgImage} display: block;"
-  ></span>
   ${imageTag}
   `.trim()
 
